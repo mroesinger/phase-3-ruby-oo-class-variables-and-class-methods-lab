5949cc78d62b5108ef555594ed803d61914106f3
michaelroesinger <mroesinger@gmail.com>
2022-09-02 14:38:08 -0700

    commit tree 81222c5eb7039bdaa1acb7989446eea76100e589

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    @@genre_count = {}
+    @@artist_count = {}
+
+    def initialize(name, artist, genre) 
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genre_count(genre)||=0
+        @@genre_count(genre)+=1
+    end
+
+    def self.artist_count
+        @@artist_count(artist)||=0
+        @@artist_count(artist)=+1
+    end
+
+end
+
